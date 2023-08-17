131901f073c61565275971896c4ed463ea10dcda
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-08-17 18:31:52 -0400

    added ability to follow/unfollow other users

--- frontend/components/profileContent.tsx
+++ frontend/components/profileContent.tsx
@@ -6,11 +6,13 @@ import ProfileClucks from "@/components/profileClucks"
 
 export default function ProfileContent({user, profile}: any) {
 
+
+  
   const [menu, setMenu ] = useState("clucks")
 
   return (
     <div className="flex flex-col gap-4">
-      <ProfileHeader profile={profile} menu={menu} setMenu={setMenu}/>
+      <ProfileHeader user={user} profile={profile} menu={menu} setMenu={setMenu}/>
       {menu == "clucks" && <ProfileClucks user={user} profile={profile}/>}
       <div>
 
--- frontend/components/profileHeader.tsx
+++ frontend/components/profileHeader.tsx
@@ -1,40 +1,91 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 
 import Link from 'next/link'
+import { Button } from './ui/button'
 
-export default function ProfileHeader({profile, menu, setMenu}: any) {
+export default function ProfileHeader({ user, profile, menu, setMenu }: any) {
 
-  return (
-    <Card className="border-black rounded-2xl border-2 border-b-4 border-r-4 shadow-xl">
-    <div className="flex flex-row">
-    <CardHeader>
-        <CardTitle><Link className="hover:underline" href={`/user/${profile.id}`}>{profile.id}</Link></CardTitle>
-        <CardDescription>{profile.name} </CardDescription>
-    </CardHeader>
-    <CardHeader className="ml-auto">
-        <CardTitle className="flex flex-row gap-2 sm:gap-8 sm:mr-12">
-            <p className="text-xs">Followers 21</p>
-            <p className="text-xs">Following 21</p>
-        </CardTitle>
+    const isMyProfile = user.id == profile.id ? true : false
+    const [isFollowing, setIsFollowing] = useState(false)
 
-    </CardHeader>
-    </div>
-			<CardContent>
-				<p>{profile.about}</p>
-			</CardContent>
-			<CardFooter className="flex flex-row gap-4 justify-center">
-                <button onClick={() => setMenu('clucks')}  className={ menu === "clucks" ?  "font-semibold" : "hover:underline"}>clucks</button>
-                <button onClick={() => setMenu('likes')}   className={ menu === "likes" ?   "font-semibold" : "hover:underline"}>likes</button>
-                <button onClick={() => setMenu('replies')} className={ menu === "replies" ? "font-semibold" : "hover:underline"}>replies</button>
-			</CardFooter>
-		</Card >
-	)
+    const followHandler = async (e: any) => {
+        e.preventDefault()
+        const updatedFollowing = !isFollowing
+		if (updatedFollowing) {
+			const requestOptions = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ user_id: user.id, following: profile.id })
+			}
+			const response = await fetch(`http://127.0.0.1:9000/api/follow_detail/`, requestOptions)
+			const data = await response.json()
+			if (response.status == 201) {
+                console.log('calling setter')
+				setIsFollowing(updatedFollowing)
+			}
+		} else {
+			// this means they unfollowed, we need to send a delete
+			const requestOptions = {
+				method: 'DELETE',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ user_id: user.id, following: profile.id })
+			}
+			const response = await fetch(`http://127.0.0.1:9000/api/follow_detail/`, requestOptions)
+			if (response.status == 204) {
+				setIsFollowing(updatedFollowing)
+			}
+		}
+	}
+
+
+    useEffect(() => {
+        // check if the user has previously like this cluck, and setLiked accordingly
+        const getFollowing = async () => {
+            const response = await fetch(`http://127.0.0.1:9000/api/follow_detail/?user_id=${user.id}&following=${profile.id}`)
+            const data = await response.json()
+            if (data?.status_code == 200) {
+                console.log(data)
+                setIsFollowing(true)
+            }
+        }
+        getFollowing()
+    }, [])
+
+    console.log(isFollowing)
+    return (
+        <Card className="border-black rounded-2xl border-2 border-b-4 border-r-4 shadow-xl">
+            <div className="flex flex-row">
+                <CardHeader>
+                    <CardTitle><Link className="hover:underline" href={`/user/${profile.id}`}>{profile.id}</Link></CardTitle>
+                    <CardDescription>{profile.name} </CardDescription>
+                </CardHeader>
+                <CardHeader className="ml-auto">
+                    <CardTitle className="flex flex-row gap-2 sm:gap-8 sm:mr-12">
+                        {isFollowing ?
+                            <Button id='following' onClick={followHandler}> Following </Button>
+                            :
+                            <Button id='follow' onClick={followHandler}> Follow </Button>
+
+                        }
+                    </CardTitle>
+                </CardHeader>
+            </div>
+            <CardContent>
+                <p>{profile.about}</p>
+            </CardContent>
+            <CardFooter className="flex flex-row gap-4 justify-center">
+                <button onClick={() => setMenu('clucks')} className={menu === "clucks" ? "font-semibold" : "hover:underline"}>clucks</button>
+                <button onClick={() => setMenu('likes')} className={menu === "likes" ? "font-semibold" : "hover:underline"}>likes</button>
+                <button onClick={() => setMenu('replies')} className={menu === "replies" ? "font-semibold" : "hover:underline"}>replies</button>
+            </CardFooter>
+        </Card >
+    )
 }
