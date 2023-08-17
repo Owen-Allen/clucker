e1185147b2e8296ace0d19e70bfc58d7ed062cf1
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-08-17 18:22:49 -0400

    added redirect to login page if the user's session expires

---
+++ frontend/components/login.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import GoogleSignInButton from './googleSignInButton'
+
+export default function Login() {
+  return (
+    <div>
+        <GoogleSignInButton/>
+    </div>
+  )
+}
