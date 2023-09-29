5283ffa8a8e18f339f4af4f344c0b9541a9ac303
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-09-28 20:13:31 -0400

    added id_token

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -6,6 +6,7 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import FeedClucks from '@/components/feedClucks'
 import { Modal } from '@/components/modal'
 import CluckForm from '@/components/cluckForm'
+import Nav from '@/components/nav'
 
 interface Props {
   searchParams : Record<string,string> | null | undefined
@@ -13,17 +14,25 @@ interface Props {
 
 export default async function Page({ searchParams } : Props ) {
   const session = await getServerSession(authOptions)
+  const user = session?.user
 
   if(!session){
     redirect('/signin')
   }
 
+  if(user && user?.email && !user?.id){
+    // signed in, but we don't have an account with this email
+    redirect('/signup')
+  }
+
+  // changed px-16 in main to px-8
   return (
     <>
-    <main className="flex min-h-screen flex-col items-center justify-between py-4 px-16 bg-sky-400">
-       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <Nav user_id={session.user.id}/>
+    <main className="flex h-max min-h-screen flex-col items-center justify-between py-4 px-8 bg-sky-400"> 
+       <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
          {!!searchParams?.cluckModal && <Modal> <CluckForm user={session.user}/> </Modal>}
-         {session && <FeedClucks user={session.user}/>}
+         {session && <FeedClucks session={session} />}
       </div>
     </main>
     </>
