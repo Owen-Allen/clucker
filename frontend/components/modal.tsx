a9f83fb8d73b7647b5a31de24dbe01ae29b0841e
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-09-28 20:05:50 -0400

    changed bg blur effect

--- frontend/components/modal.tsx
+++ frontend/components/modal.tsx
@@ -6,11 +6,11 @@ this allows the nav to remain a server component
 */}
 export function Modal({ children }: { children: React.ReactNode; }) {
     return (
-        <div className="relative z-10" aria-modal="true">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75"/>
-            <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="relative z-50" aria-modal="true">
+            <div className="fixed inset-0 backdrop-blur-sm	"/> {/*  */}
+            <div className="fixed inset-0 z-50 overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center items-center">  {/* controls where it is positioned on */}
-                    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all overflow-auto sm:my-8 w-full sm:max-w-lg">
+                    <div className="border-2 border-black relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all overflow-auto sm:my-8 w-full sm:max-w-lg">
                         <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                                 {children}
                         </div>
