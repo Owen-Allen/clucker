6c4a9548a19c838412815f308f8984fd72827d3e
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-10-02 17:17:12 -0400

    removed duplicate hr element

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -43,7 +43,6 @@ export default async function Page() {
           <GoogleSignInButton />
         </div>
         <hr className="border-b border-gray-300" />
-        <hr className="border-b border-gray-300" />
         <div className="text-center pt-6">
           <a className='mx-auto block'> Don&apos;t have an account?</a>
           <SignUpButton />
@@ -52,23 +51,3 @@ export default async function Page() {
     </div>
   )
 }
-    // <div className="grid h-screen place-items-center">
-    //   <div className="bg-white border-black rounded-2xl border-2 border-b-4 border-r-4 shadow-xl p-8 sm:px-16">
-    //     <h1 className="font-mono text-xl font-semibold text-center p-4">Welcome to <span className="text-2xl">Clucker!</span></h1>
-    //     <Image
-    //       className="rounded w-16 mx-auto pb-4"
-    //       src={"/chicken.png"}
-    //       width='64'
-    //       height='64'
-    //       alt="Clucker Logo"
-    //     />
-    // <div className="py-4">
-    //       <GoogleSignInButton/>
-    //     </div>
-    //     <hr className="border-b border-gray-300"/>
-    //     <div className="text-center pt-6">
-    //       <a className='mx-auto block'> Don&apos;t have an account?</a>
-    //       <a href="/signup" className='mx-auto underline'>create an account </a>
-    //     </div>
-    //     </div>
-    // </div>
